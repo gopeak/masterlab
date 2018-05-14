a0fa57791cb0bc567a81485bf410efb8c7b0ab0f
sven <121642038@qq.com>
2018-05-15 00:46:44 +0800

    update database.cfg.php

--- app/config/development/database.cfg.php
+++ app/config/development/database.cfg.php
@@ -1,59 +1,58 @@
-<?php 
- $_config = array (
-  'database' => 
-  array (
-    'default' => 
-    array (
-      'driver' => 'mysql',
-      'host' => '127.0.0.1',
-      'port' => '3306',
-      'user' => 'root',
-      'password' => '',
-      'db_name' => 'hornet',
-      'charset' => 'utf8',
-      'timeout' => 10,
-      'show_field_info' => false,
-    ),
-    'framework_db' => 
-    array (
-      'driver' => 'mysql',
-      'host' => '127.0.0.1',
-      'port' => '3306',
-      'user' => 'root',
-      'password' => '',
-      'db_name' => 'xphp_test',
-      'charset' => 'utf8',
-      'timeout' => 10,
-      'show_field_info' => false,
-    ),
-    'log_db' => 
-    array (
-      'driver' => 'mysql',
-      'host' => '112.74.12.52',
-      'port' => '3306',
-      'user' => 'hornet',
-      'password' => 'hornet2017@.@',
-      'db_name' => 'hornet',
-      'charset' => 'utf8',
-      'timeout' => 10,
-      'show_field_info' => false,
-    ),
-  ),
-  'config_map_class' => 
-  array (
-    'default' => 
-    array (
-    ),
-    'framework_db' => 
-    array (
-      0 => 'FrameworkUserModel',
-      1 => 'FrameworkCacheKeyModel',
-    ),
-    'log_db' => 
-    array (
-      0 => 'UnitTestUnitModel'
-    ),
-  ),
+<?php
+$_config = array(
+    'database' =>
+        array(
+            'default' =>
+                array(
+                    'driver' => 'mysql',
+                    'host' => '112.74.12.52',
+                    'port' => '3306',
+                    'user' => 'hornet',
+                    'password' => 'hornet2017@.@',
+                    'db_name' => 'hornet',
+                    'charset' => 'utf8',
+                    'timeout' => 10,
+                    'show_field_info' => false,
+                ),
+            'framework_db' =>
+                array(
+                    'driver' => 'mysql',
+                    'host' => '127.0.0.1',
+                    'port' => '3306',
+                    'user' => 'root',
+                    'password' => '',
+                    'db_name' => 'xphp_test',
+                    'charset' => 'utf8',
+                    'timeout' => 10,
+                    'show_field_info' => false,
+                ),
+            'log_db' =>
+                array(
+                    'driver' => 'mysql',
+                    'host' => '112.74.12.52',
+                    'port' => '3306',
+                    'user' => 'hornet',
+                    'password' => 'hornet2017@.@',
+                    'db_name' => 'hornet',
+                    'charset' => 'utf8',
+                    'timeout' => 10,
+                    'show_field_info' => false,
+                ),
+        ),
+    'config_map_class' =>
+        array(
+            'default' =>
+                array(),
+            'framework_db' =>
+                array(
+                    0 => 'FrameworkUserModel',
+                    1 => 'FrameworkCacheKeyModel',
+                ),
+            'log_db' =>
+                array(
+                    0 => 'UnitTestUnitModel'
+                ),
+        ),
 );
 
 return $_config;
