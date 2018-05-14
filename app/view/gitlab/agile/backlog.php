3da46732836af6ffd73d411925dcac2717975495
sven <121642038@qq.com>
2018-05-14 20:30:59 +0800

    update backlog page

--- app/view/gitlab/agile/backlog.php
+++ app/view/gitlab/agile/backlog.php
@@ -4,7 +4,6 @@
 
     <? require_once VIEW_PATH . 'gitlab/common/header/include.php'; ?>
 
-    <script src="<?= ROOT_URL ?>gitlab/assets/webpack/filtered_search.bundle.js"></script>
     <script src="<?= ROOT_URL ?>dev/lib/moment.js"></script>
     <script src="<?= ROOT_URL ?>dev/lib/url_param.js" type="text/javascript" charset="utf-8"></script>
     <script src="<?= ROOT_URL ?>dev/js/agile/backlog.js" type="text/javascript" charset="utf-8"></script>
@@ -16,8 +15,6 @@
     </script>
 
     <script src="<?=ROOT_URL?>dev/lib/bootstrap-paginator/src/bootstrap-paginator.js"  type="text/javascript"></script>
-    <script type="text/javascript" src="<?=ROOT_URL?>dev/lib/qtip/dist/jquery.qtip.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="<?=ROOT_URL?>dev/lib/qtip/dist/jquery.qtip.min.css" />
 
     <script src="<?= ROOT_URL ?>dev/lib/mousetrap/mousetrap.min.js"></script>
 
@@ -50,206 +47,20 @@
         <div class=" ">
             <div class="content" id="content-body">
                 <div class="container-fluid">
-                    <div class="issues-filters">
-                        <div class="filtered-search-block issues-details-filters row-content-block second-block"
-                             v-pre="false">
-                            <form class="filter-form js-filter-form" action="#" accept-charset="UTF-8" method="get">
-                                <input name="utf8" type="hidden" value="&#x2713;"/>
-                                <input name="page" id="filter_page" type="hidden" value="1">
-                                <div class="check-all-holder">
-                                    <input type="checkbox" name="check_all_issues" id="check_all_issues"
-                                           class="check_all_issues left"/>
-                                </div>
-                                <div class="issues-other-filters filtered-search-wrapper">
-                                    <div class="filtered-search-box">
-                                        <div class="dropdown filtered-search-history-dropdown-wrapper">
-                                            <button class="dropdown-menu-toggle filtered-search-history-dropdown-toggle-button"
-                                                    type="button" data-toggle="dropdown">
-                                                <span class="dropdown-toggle-text "><i class="fa fa-history"></i></span><i
-                                                        class="fa fa-chevron-down"></i>
-                                            </button>
-                                            <div class="dropdown-menu dropdown-select filtered-search-history-dropdown">
-                                                <div class="dropdown-title"><span>Recent searches</span>
-                                                    <button class="dropdown-title-button dropdown-menu-close"
-                                                            aria-label="Close" type="button"><i
-                                                                class="fa fa-times dropdown-menu-close-icon"></i>
-                                                    </button>
-                                                </div>
-                                                <div class="dropdown-content filtered-search-history-dropdown-content">
-                                                    <div class="js-filtered-search-history-dropdown"></div>
-                                                </div>
-                                                <div class="dropdown-loading"><i class="fa fa-spinner fa-spin"></i>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="filtered-search-box-input-container">
-                                            <div class="scroll-container">
-                                                <ul class="tokens-container list-unstyled">
-                                                    <li class="input-token">
-                                                        <input class="form-control filtered-search"
-                                                               data-base-endpoint="/ismond/xphp" data-project-id="31"
-                                                               data-username-params="[]" id="filtered-search-issues"
-                                                               placeholder="Search or filter results...">
-                                                    </li>
-                                                </ul>
-                                                <i class="fa fa-filter"></i>
-                                                <button class="clear-search hidden" type="button">
-                                                    <i class="fa fa-times"></i>
-                                                </button>
-                                            </div>
-                                            <div class="filtered-search-input-dropdown-menu dropdown-menu hint-dropdown"
-                                                 id="js-dropdown-hint">
-                                                <ul data-dropdown>
-                                                    <li class="filter-dropdown-item" data-action="submit">
-                                                        <button class="btn btn-link">
-                                                            <i class="fa fa-search"></i>
-                                                            <span>回车或点击搜索</span>
-                                                        </button>
-                                                    </li>
-                                                </ul>
-                                                <ul class="filter-dropdown" data-dropdown data-dynamic>
-                                                    <li class="filter-dropdown-item">
-                                                        <button class="btn btn-link">
-                                                            <i class="fa {{icon}}"></i>
-                                                            <span class="js-filter-hint">{{hint}}</span>
-                                                            <span class="js-filter-tag dropdown-light-content">{{tag}}</span>
-                                                        </button>
-                                                    </li>
-                                                </ul>
-                                            </div>
+                    <div class="nav-block activity-filter-block">
+                        <div class="controls">
+                            <a title="Subscribe" class="btn rss-btn has-tooltip" href="/ismond/b2b.atom?private_token=vyxEf937XeWRN9gDqyXk"><i class="fa fa-rss"></i>
+                            </a></div>
+                        <ul class="nav-links event-filter scrolling-tabs is-initialized">
+                            <li class="active"><a class="event-filter-link" id="all_event_filter" title="Filter by all" href="/ismond/b2b"><span> All</span></a></li>
+                            <li class=""><a class="event-filter-link" id="push_event_filter" title="Filter by push events" href="/ismond/b2b"><span> Push events</span></a></li>
+                            <li class=""><a class="event-filter-link" id="merged_event_filter" title="Filter by merge events" href="/ismond/b2b"><span> Merge events</span></a></li>
+                            <li class=""><a class="event-filter-link" id="issue_event_filter" title="Filter by issue events" href="/ismond/b2b"><span> Issue events</span></a></li>
+                            <li class=""><a class="event-filter-link" id="comments_event_filter" title="Filter by comments" href="/ismond/b2b"><span> Comments</span></a></li>
+                            <li><a class="event-filter-link" id="team_event_filter" title="Filter by team" href="/ismond/b2b"><span> Team</span></a></li>
+                        </ul>
 
-
-                                            <div class="filtered-search-input-dropdown-menu dropdown-menu"
-                                                 data-hint="author" data-icon="pencil" data-tag="@author"
-                                                 id="js-dropdown-author">
-                                                <ul class="filter-dropdown" data-dropdown data-dynamic>
-                                                    <li class="filter-dropdown-item">
-                                                        <button class="btn btn-link dropdown-user">
-                                                            <img alt="{{name}}&#39;s avatar" class="avatar"
-                                                                 data-src="{{avatar_url}}" width="30">
-                                                            <div class="dropdown-user-details"><span>{{name}}</span>
-                                                                <span class="dropdown-light-content">@{{username}}</span>
-                                                            </div>
-                                                        </button>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                            <div class="filtered-search-input-dropdown-menu dropdown-menu"
-                                                 data-hint="assignee" data-icon="user" data-tag="@assignee"
-                                                 id="js-dropdown-assignee">
-                                                <ul data-dropdown>
-                                                    <li class="filter-dropdown-item" data-value="none">
-                                                        <button class="btn btn-link">
-                                                            No Assignee
-                                                        </button>
-                                                    </li>
-                                                    <li class="divider"></li>
-                                                </ul>
-                                                <ul class="filter-dropdown" data-dropdown data-dynamic>
-                                                    <li class="filter-dropdown-item">
-                                                        <button class="btn btn-link dropdown-user">
-                                                            <img alt="{{name}}&#39;s avatar" class="avatar"
-                                                                 data-src="{{avatar_url}}" width="30">
-                                                            <div class="dropdown-user-details"><span>{{name}}</span>
-                                                                <span class="dropdown-light-content">@{{username}}</span>
-                                                            </div>
-                                                        </button>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                            <div class="filtered-search-input-dropdown-menu dropdown-menu"
-                                                 data-hint="module" data-icon="square" data-tag="module"
-                                                 data-type="input" id="js-dropdown-module">
-                                                <ul data-dropdown>
-                                                    <li class="filter-dropdown-item" data-value="none">
-                                                        <button class="btn btn-link">
-                                                            No Module
-                                                        </button>
-                                                    </li>
-                                                    <li class="divider"></li>
-                                                </ul>
-                                                <ul class="filter-dropdown" data-dropdown data-dynamic>
-                                                    <li class="filter-dropdown-item">
-                                                        <button class="btn btn-link">
-                                                            <span class="label-title js-data-value">{{name}}</span>
-                                                        </button>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                            <div class="filtered-search-input-dropdown-menu dropdown-menu"
-                                                 data-hint="status" data-icon="info" data-tag="status" data-type="input"
-                                                 id="js-dropdown-status">
-
-                                                <ul class="filter-dropdown" data-dropdown data-dynamic>
-                                                    <li class="filter-dropdown-item">
-                                                        <button class="btn btn-link">
-                                                            <span class="label label-{{color}}   js-data-value">{{name}}</span>
-                                                        </button>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                            <div class="filtered-search-input-dropdown-menu dropdown-menu"
-                                                 data-hint="resolve" data-icon="info" data-tag="resolve"
-                                                 data-type="input" id="js-dropdown-resolve">
-
-                                                <ul class="filter-dropdown" data-dropdown data-dynamic>
-                                                    <li class="filter-dropdown-item">
-                                                        <button class="btn btn-link">
-                                                            <span style="color:{{color}}"
-                                                                  class="label-title js-data-value">{{name}}</span>
-                                                        </button>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                            <div class="filtered-search-input-dropdown-menu dropdown-menu"
-                                                 data-hint="priority" data-icon="info" data-tag="priority"
-                                                 data-type="input" id="js-dropdown-priority">
-
-                                                <ul class="filter-dropdown" data-dropdown data-dynamic>
-                                                    <li class="filter-dropdown-item">
-                                                        <button class="btn btn-link">
-                                                            <span style="color:{{status_color}}"
-                                                                  class="label-title js-data-value">{{name}}</span>
-                                                        </button>
-                                                    </li>
-                                                </ul>
-                                            </div>
-
-                                        </div>
-                                    </div>
-                                    <div class="filter-dropdown-container">
-                                        <div class="dropdown inline prepend-left-10">
-
-                                            <button class="dropdown-toggle"  id="save_filter-btn"  type="button">
-                                                <i class="fa fa-filter "></i> Save Filter
-                                            </button>
-
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </form>
-
-                        </div>
                     </div>
-                    <script>
-                        new UsersSelect();
-                        new LabelsSelect();
-                        new MilestoneSelect();
-                        new IssueStatusSelect();
-                        new SubscriptionSelect();
-
-                        $(document).off('page:restore').on('page:restore', function (event) {
-                            if (gl.FilteredSearchManager) {
-                                new gl.FilteredSearchManager();
-                            }
-                            Issuable.init();
-                            new gl.IssuableBulkActions({
-                                prefixId: 'issue_',
-                            });
-                        });
-                    </script>
-
                     <div class="issues-holder">
                         <div class="table-holder">
                             <table class="table  tree-table" id="tree-slider">
