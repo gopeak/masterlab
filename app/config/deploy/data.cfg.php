b4e67d6903d2e1888d8c18101687a1502e704d5f
lijian <23335096@qq.com>
2018-06-20 11:51:56 +0800

    define custom dev && add env.ini

---
+++ app/config/deploy/data.cfg.php
@@ -0,0 +1,15 @@
+<?php
+
+$_config['token']['public_key'] =  '783749283939mcvnkwdmuodk29493';
+
+$_config['token']['secret_key'] =  'sadf78374mcvjsakqowenudu92dfj83939mcvnkwdmuodk294cgdg93';
+
+$_config['token']['expire_time'] =  3600*24*30;
+$_config['token']['refresh_expire_time'] =  3600*24*30;
+
+$_config['login']['pass_phrase'] =  'gohome@#fine';
+
+return $_config ;
+     
+
+ 
