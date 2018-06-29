30e8df5932f08aea5aa8f8f4a1a55b458e41473f
lyman <cheerfultony@gmail.com>
2018-06-29 09:56:40 +0800

    delete debug

--- app/ctrl/Projects.php
+++ app/ctrl/Projects.php
@@ -104,53 +104,10 @@ class Projects extends BaseUserCtrl
 
     public function test()
     {
-
-        set_time_limit(0);
-        ignore_user_abort(true);
-
         echo (new SettingsLogic)->dateTimezone();
-        $dbConfig = getConfigVar('database');
-        $dbConfig = $dbConfig['database']['default'];
-
-        $time = -microtime(true);
-
-        $dump = new \main\lib\MySqlDump($dbConfig);
-        $dumpFile = STORAGE_PATH .'dump ' . date('Y-m-d H-i') . '.sql.gz';
-        $dumpFile = STORAGE_PATH .'dump_test.sql.gz';
-        $dump->save($dumpFile);
-
-        $time += microtime(true);
-        echo "FINISHED (in $time s)";
     }
 
-    public function test2()
-    {
 
-        set_time_limit(0);
-        ignore_user_abort(true);
-
-        $dbConfig = getConfigVar('database');
-        $dbConfig = $dbConfig['database']['default'];
-
-        $dumpFile = STORAGE_PATH .'dump_test.sql.gz';
-
-        $time = -microtime(true);
-
-        $import = new \main\lib\MySqlImport($dbConfig);
-
-        $import->onProgress = function ($count, $percent) {
-            if ($percent !== null) {
-                echo (int) $percent . " %\r";
-            } elseif ($count % 10 === 0) {
-                echo '.';
-            }
-        };
-
-        $import->load($dumpFile);
-
-        $time += microtime(true);
-        echo "FINISHED (in $time s)";
-    }
 
 
 }
