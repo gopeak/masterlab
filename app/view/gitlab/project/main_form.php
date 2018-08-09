3989e81ed7e0bd7efa153c2731d1ec23d13cba5a
zhaolong <4282742@qq.com>
2018-08-09 18:00:58 +0800

    #

--- app/view/gitlab/project/main_form.php
+++ app/view/gitlab/project/main_form.php
@@ -154,9 +154,9 @@
                         </div>
                     </div>
 
-                    <div class="form-actions">
+                    <div class="form-actions" style="text-align: right;">
                         <input type="submit" name="commit" value="创建项目" class="btn btn-create disabled">
-                        <a class="btn btn-cancel" href="/projects">取消</a>
+                        <a class="btn btn-cancel" href="/projects" style="float: none">取消</a>
                     </div>
                 </form>
 
