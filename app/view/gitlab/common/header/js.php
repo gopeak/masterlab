f0b295c8dff904ead7ddbc8da9229b098de9ad64
sven <121642038@qq.com>
2018-08-20 01:36:09 +0800

    去掉hotjar代码

--- app/view/gitlab/common/header/js.php
+++ app/view/gitlab/common/header/js.php
@@ -10,14 +10,3 @@
     window.preview_markdown_path = "/issue/main/preview_markdown";
 
 </script>
-<!-- Hotjar Tracking Code for http://wx.888zb.com -->
-<script>
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:978585,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
