c0b4a456d9dd408bea8ea1e90706362644925ba2
jugg.walker <jugg.walker@qq.com>
2018-09-04 22:01:40 +0800

    projectmodule unit test finished

--- app/test/unit/classes/TestProjectModuleFilterLogic.php
+++ app/test/unit/classes/TestProjectModuleFilterLogic.php
@@ -3,6 +3,7 @@
 namespace main\app\test\unit\classes;
 
 use main\app\classes\ProjectModuleFilterLogic;
+use main\app\model\project\ProjectModel;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -22,16 +23,58 @@ class TestProjectModuleFilterLogic extends TestCase
 
     public function testGetModuleByFilter()
     {
-        $this->markTestIncomplete('TODO:' . __METHOD__);
+        $model = new ProjectModel();
+        $ret = $model->getAll();
+        if (count($ret) > 0) {
+            $projectIdArr = array_keys($ret);
+            $keySeed = mt_rand(0, count($projectIdArr)-1);
+            $projectId = $projectIdArr[$keySeed];
+
+            $model = new ProjectModuleFilterLogic();
+            list($flag, $data, $count) = $model->getModuleByFilter($projectId);
+            $this->assertTrue(is_array($data));
+            $this->assertEquals(count($data), $count);
+        } else {
+            $this->markTestIncomplete('因为没有项目数据,所以忽略该单元测试.');
+        }
     }
 
     public function testGetByProjectWithUser()
     {
-        $this->markTestIncomplete('TODO:' . __METHOD__);
+        $model = new ProjectModel();
+        $ret = $model->getAll();
+        if (count($ret) > 0) {
+            $projectIdArr = array_keys($ret);
+            $keySeed = mt_rand(0, count($projectIdArr)-1);
+            $projectId = $projectIdArr[$keySeed];
+
+            $model = new ProjectModuleFilterLogic();
+            $ret = $model->getByProjectWithUser($projectId);
+            $this->assertTrue(is_array($ret));
+        } else {
+            $this->markTestIncomplete('因为没有项目数据,所以忽略该单元测试.');
+        }
     }
 
     public function testGetByProjectWithUserLikeName()
     {
-        $this->markTestIncomplete('TODO:' . __METHOD__);
+        $model = new ProjectModel();
+        $ret = $model->getAll();
+        if (count($ret) > 0) {
+            $projectIdArr = array_keys($ret);
+            $keySeed = mt_rand(0, count($projectIdArr)-1);
+            $projectId = $projectIdArr[$keySeed];
+
+            $model = new ProjectModuleFilterLogic();
+            $ret = $model->getByProjectWithUserLikeName($projectId, '');
+            $this->assertTrue(is_array($ret));
+
+            $randString = 'JUGG-UNIT-TEST-' . quickRandom(10);
+            $ret = $model->getByProjectWithUserLikeName($projectId, $randString);
+            $this->assertTrue(is_array($ret));
+            $this->assertEmpty($ret);
+        } else {
+            $this->markTestIncomplete('因为没有项目数据,所以忽略该单元测试.');
+        }
     }
 }
