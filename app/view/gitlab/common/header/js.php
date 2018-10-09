5db71a8b96be6be8b20ea48c9ae01a5a05f6f544
zhaolong <4282742@qq.com>
2018-10-09 18:03:32 +0800

    重构调用

--- app/view/gitlab/common/header/js.php
+++ app/view/gitlab/common/header/js.php
@@ -3,9 +3,16 @@
 <script src="<?= ROOT_URL ?>gitlab/assets/webpack/main.bundle.js"></script>
 <script src="<?= ROOT_URL ?>dev/lib/notify/bootstrap-notify/dist/bootstrap-notify.min.js"></script>
 <!-- recoding -->
-<!-- <script src="<?= ROOT_URL ?>recoding/lib/jquery.min.js"></script>
+<!-- 
+<script src="<?= ROOT_URL ?>recoding/lib/jquery.min.js"></script>
 <script src="<?= ROOT_URL ?>recoding/lib/bootstrap.min.js"></script>
-<script src="<?= ROOT_URL ?>recoding/components/tooltip.js"></script> -->
+<script src="<?= ROOT_URL ?>recoding/components/tooltip.js"></script>
+<script>
+	    $(function () {
+			$('[data-toggle="tooltip"]').tooltip()
+		})
+</script>
+ -->
 <!-- end -->
 <script src="<?= ROOT_URL ?>dev/js/logo.js"></script>
 <script src="<?= ROOT_URL ?>dev/js/function.helper.js"></script>
@@ -16,7 +23,5 @@
 
     window.project_uploads_path = "/issue/main/upload";
     window.preview_markdown_path = "/issue/main/preview_markdown";
-    $(function () {
-		$('[data-toggle="tooltip"]').tooltip()
-	})
+    
 </script>
