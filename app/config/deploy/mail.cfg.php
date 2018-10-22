94529f30ffa1731e594668fce7a2b2ef32dffd2c
sven <121642038@qq.com>
2018-10-22 17:14:30 +0800

    修改邮件配置

--- app/config/deploy/mail.cfg.php
+++ app/config/deploy/mail.cfg.php
@@ -5,13 +5,13 @@ $_config = array(
 
     'host' => 'smtp.vip.163.com',
     'port' => 25,
-    'from' => array('address' => 'ismond@vip.163.com', 'name' => 'Administrator'),
+    'from' => array('address' => 'xxx@vip.163.com', 'name' => 'Administrator'),
     'encryption' => 'ssl',
-    'username' => 'ismond@vip.163.com',
-    'password' => 'ismond163vip',
+    'username' => 'xxx@vip.163.com',
+    'password' => 'xxx163vip',
     'sendmail' => '/usr/sbin/sendmail -bs', 
     // 管理员邮箱 
-    'amdin_email' => 'ismond@vip.163.com',
+    'amdin_email' => 'xxxx@vip.163.com',
     'timeout'=>16
 );
 
