43380986d025f0512738f84f7e7d7a13dd58e914
sven <121642038@qq.com>
2018-11-13 13:25:44 +0800

    test travis sh

--- app/config/travis/cache.cfg.php
+++ app/config/travis/cache.cfg.php
@@ -1,8 +1,8 @@
 <?php
 
     $_config = [];
-    $_config['redis']['data'] =  [ [ '127.0.0.1', 7379] ]  ;
-    $_config['redis']['session'] =  [ [ '127.0.0.1', 7379] ]  ;
+    $_config['redis']['data'] =  [ [ '127.0.0.1', 6379] ]  ;
+    $_config['redis']['session'] =  [ [ '127.0.0.1', 6379] ]  ;
     $_config['mongodb']['server'] =  array( '127.0.0.1', 27017  , 'mongodb' );
 
     $_config['enable'] = false;
