18b6b1e0b6d110b973617af975d4e7fbc1644061
sven <121642038@qq.com>
2018-11-15 20:06:39 +0800

    update .travis.yml

--- app/public/test_mysql.php
+++ app/public/test_mysql.php
@@ -1,7 +1,6 @@
 <?php
 
 
-
 #print_r($_SERVER);
 
 $mysqli = new mysqli("localhost", "root", "Masterlab123", "masterlab_ci");
@@ -12,35 +11,13 @@ if (mysqli_connect_errno()) {
     exit();
 }
 
-/* Insert rows */
-$mysqli->query("CREATE TABLE Language SELECT * from CountryLanguage");
-printf("Affected rows (INSERT): %d\n", $mysqli->affected_rows);
 
-$mysqli->query("ALTER TABLE Language ADD Status int default 0");
-
-/* update rows */
-$mysqli->query("UPDATE Language SET Status=1 WHERE Percentage > 50");
-printf("Affected rows (UPDATE): %d\n", $mysqli->affected_rows);
-
-/* delete rows */
-$mysqli->query("DELETE FROM Language WHERE Percentage < 50");
-printf("Affected rows (DELETE): %d\n", $mysqli->affected_rows);
-
-/* select all rows */
-$result = $mysqli->query("SELECT CountryCode FROM Language");
-printf("Affected rows (SELECT): %d\n", $mysqli->affected_rows);
-
-/* Delete table Language */
-$mysqli->query("DROP TABLE Language");
-
-#$result = $mysqli->query("Select @@sql_mode");
-
-$query = " SHOW VARIABLES LIKE 'sql_%' ";
-$result = $mysqli->query($query);
-
-$row = $result->fetch_array(MYSQLI_NUM);
-#printf ("%s (%s)\n", $row[0], $row[1]);
+$query = "Select @@sql_mode";
+$result = $mysqli->query($query);
+$row = $result->fetch_array(MYSQLI_NUM);
 print_r($row);
+
+echo $mysqli->get_server_info();
 
 /* close connection */
 $mysqli->close();
