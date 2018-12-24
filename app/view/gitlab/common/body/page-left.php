5fb8941ced739a7852500b3feedd9339cd3118fd
谢松青 <1043423813@qq.com>
2018-12-24 17:12:47 +0800

    项目图标和项目名称样式调整

--- app/view/gitlab/common/body/page-left.php
+++ app/view/gitlab/common/body/page-left.php
@@ -6,8 +6,6 @@ if (isset($G_Preferences['scheme_style']) && !empty($G_Preferences['scheme_style
 if($layout=='left'){
     require_once VIEW_PATH . 'gitlab/common/body/layout/' . $layout . '/page-left.php';
 }
-
-
 ?>
 
 
