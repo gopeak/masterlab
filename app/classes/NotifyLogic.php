dd879c1d7c0f6a54a5c029cfdaef1f41c00e3113
jugg.walker <jugg.walker@qq.com>
2019-03-06 19:03:41 +0800

    debug

--- app/classes/NotifyLogic.php
+++ app/classes/NotifyLogic.php
@@ -2,6 +2,10 @@
 namespace main\app\classes;
 
 
+use main\app\model\agile\SprintModel;
+use main\app\model\issue\IssueFollowModel;
+use main\app\model\issue\IssueModel;
+use main\app\model\project\ProjectModel;
 use main\app\model\system\NotifySchemeDataModel;
 use main\app\model\system\NotifySchemeModel;
 
@@ -12,14 +16,23 @@ class NotifyLogic
     public $subject = null;
     public $body = null;
 
+    const NOTIFY_ROLE_ASSIGEE = 'assigee';
+    const NOTIFY_ROLE_REPORTER = 'reporter';
+    const NOTIFY_ROLE_FOLLOW = 'follow';
+    const NOTIFY_ROLE_PROJECT = 'project';
+
     public function __construct()
     {
     }
 
     /**
+     * @param string $schemeDataFlag    标识
+     * @param int $projectId            项目ID
+     * @param int $sourceId             资源ID(issueid or sprintid)
+     * @param string $body              邮件内容html, 如果为空自动用标题代替
      * @throws \Exception
      */
-    public function send($schemeDataFlag, $data = ['project_id' => 0, ''])
+    public function send($schemeDataFlag, $projectId, $sourceId = 0, $body = '')
     {
         $syncFlag = 1;
         $toEmails = [];
@@ -30,24 +43,48 @@ class NotifyLogic
         $flagUserRoleMap = array_column($notifySchemeDataResult, 'user', 'flag');
         $flagNameMap = array_column($notifySchemeDataResult, 'name', 'flag');
 
+        $schemeDataFlagArr = explode('@', $schemeDataFlag);
+        $sourceType = $schemeDataFlagArr[0];
+
+        if ($sourceType == 'issue') {
+            $issueModel = new IssueModel();
+            $row = $issueModel->getById($sourceId);
+            $sourceTitle = $row['summary'];
+        }
+        if ($sourceType == 'sprint') {
+            $sprintModel = SprintModel::getInstance();
+            $row = $sprintModel->getById($sourceId);
+            $sourceTitle = $row['name'];
+        }
+
+
+
+
         $notifyRoleArr = $flagUserRoleMap[$schemeDataFlag];
 
         foreach ($notifyRoleArr as $notifyRole) {
-            if ($notifyRole == 'assigee') {
-                array_merge($toEmails, $this->getAssigeeUser());
+            if ($notifyRole == self::NOTIFY_ROLE_ASSIGEE) {
+                array_merge($toEmails, $this->getAssigeeUser($row, $sourceType));
             }
-            if ($notifyRole == 'reporter') {
-                array_merge($toEmails, $this->getReporterUser());
+            if ($notifyRole == self::NOTIFY_ROLE_REPORTER) {
+                array_merge($toEmails, $this->getReporterUser($row, $sourceType));
             }
-            if ($notifyRole == 'follow') {
-                array_merge($toEmails, $this->getFollowUser());
+            if ($notifyRole == self::NOTIFY_ROLE_FOLLOW) {
+                array_merge($toEmails, $this->getFollowUser($row, $sourceType));
             }
-            if ($notifyRole == 'project') {
-                array_merge($toEmails, $this->getReporterUser());
+            if ($notifyRole == self::NOTIFY_ROLE_PROJECT) {
+                array_merge($toEmails, $this->getProjectUser($projectId));
             }
         }
 
-        $this->sendEmailInitParams($toEmails, $flagNameMap[$schemeDataFlag], '');
+        $this->sendEmailInitParams(
+            $toEmails,
+            $sourceType,
+            $flagNameMap[$schemeDataFlag],
+            $projectId,
+            $sourceId,
+            $body
+        );
 
         if ($syncFlag) {
             $this->syncSend();
@@ -79,14 +116,20 @@ class NotifyLogic
 
     /**
      * 初始化发送邮件的参数
-     * @param $toEmails
-     * @param $subject
-     * @param $body
+     * @param array $toEmails
+     * @param string $sourceType
+     * @param string $schemeDataFlagName
+     * @param int $projectId
+     * @param int $sourceId
+     * @param string $body
+     * @throws \Exception
      */
-    private function sendEmailInitParams($toEmails, $subject, $body)
+    private function sendEmailInitParams($toEmails, $sourceType, $schemeDataFlagName, $projectId, $sourceId, $body)
     {
         $fromEmail = 'notify@masterlab.vip';
         $fromName = 'Notify';
+
+        $sourceTitle = '';
 
         if (is_array($toEmails)) {
             $to = [];
@@ -97,13 +140,30 @@ class NotifyLogic
             $to = $toEmails;
         }
 
+        if ($sourceType == 'issue') {
+            $issueModel = new IssueModel();
+            $row = $issueModel->getById($sourceId);
+            $sourceTitle = $row['summary'];
 
-        $projectPathName = 'default/DEV';
+            new IssueFollowModel();
+
+
+        }
+        if ($sourceType == 'sprint') {
+            $sprintModel = SprintModel::getInstance();
+            $row = $sprintModel->getById($sourceId);
+            $sourceTitle = $row['name'];
+        }
+
+        $projectModel = new ProjectModel();
+        $row = $projectModel->getById($projectId);
+
+        $projectPathName = $row['org_path'] . '/' . $row['key'];
 
         $this->to = is_array($to) ? implode(',', $to) : $to;
         $this->from = mb_encode_mimeheader($fromName) . ' <' . $fromEmail . '>';
-        $this->subject = sprintf('[%s] %s', $projectPathName, $subject);
-        $this->body = $body;
+        $this->subject = sprintf('[%s] %s #%s %s', $projectPathName, $schemeDataFlagName, $sourceId, $sourceTitle);
+        $this->body = empty($body) ? $this->subject : $body;
     }
 
     /**
@@ -118,42 +178,64 @@ class NotifyLogic
     }
 
     /**
-     * 获取项目成员邮箱列表
+     * 获取项目成员USERID
      * @return array
      */
-    private function getProjectUser()
+    private function getProjectUser($projectId)
     {
 
         return [];
     }
 
     /**
-     * 获取该项目经办人邮箱地址
+     * 获取该项目经办人USERID
      * @return array
      */
-    private function getAssigeeUser()
+    private function getAssigeeUser($rowData, $sourceType)
     {
+        if (empty($rowData)) {
+            return [];
+        }
 
-        return [];
+        $assigneeUserId = 0;
+        if ($sourceType == 'issue') {
+            $assigneeUserId = $rowData['assignee'];
+        }
+
+        return [$assigneeUserId];
     }
 
     /**
-     * 获取事项报告人的邮箱地址
+     * 获取事项报告人的USERID
      * @return array
      */
-    private function getReporterUser()
+    private function getReporterUser($rowData, $sourceType)
     {
+        if (empty($rowData)) {
+            return [];
+        }
 
-        return [];
+        $reporterUserId = 0;
+        if ($sourceType == 'issue') {
+            $reporterUserId = $rowData['reporter'];
+        }
+        return [$reporterUserId];
     }
 
     /**
-     * 获取事项关注者的邮箱地址
+     * 获取事项关注者USERID
      * @return array
      */
-    private function getFollowUser()
+    private function getFollowUser($rowData, $sourceType)
     {
+        if (empty($rowData)) {
+            return [];
+        }
 
+        $reporterUserId = 0;
+        if ($sourceType == 'issue') {
+            $reporterUserId = $rowData['reporter'];
+        }
         return [];
     }
 
