7d400fa315a59fa22f05e201713e76018483ba1c
sven <121642038@qq.com>
2019-03-25 19:40:32 +0800

    更新定时执行时间

--- app/public/install/data/main.sql
+++ app/public/install/data/main.sql
@@ -1488,6 +1488,9 @@ INSERT INTO `main_setting` VALUES ('61', 'company_linkman', '联系人', 'basic'
 INSERT INTO `main_setting` VALUES ('62', 'company_phone', '联系电话', 'basic', '0', '135255256544', '', 'string', 'text', null, '');
 INSERT INTO `main_setting` VALUES ('63', 'enable_async_mail', '是否使用异步方式发送邮件', 'mail', '0', '1', '1', 'int', 'radio', '{\"1\":\"开启\",\"0\":\"关闭\"}', '');
 INSERT INTO `main_setting` VALUES ('64', 'enable_mail', '是否开启邮件推送', 'mail', '0', '1', '1', 'int', 'radio', '{\"1\":\"开启\",\"0\":\"关闭\"}', '');
+INSERT INTO `main_setting` (`id`, `_key`, `title`, `module`, `order_weight`, `_value`, `default_value`, `format`, `form_input_type`, `form_optional_value`, `description`) VALUES
+(NULL, 'socket_server_host', 'MasterlabSocket服务器地址', 'mail', '0', '127.0.0.1', '127.0.0.1', 'string', 'text', NULL, ''),
+(NULL, 'socket_server_port', 'MasterlabSocket服务器端口', 'mail', '0', '9002', '9002', 'int', 'text', NULL, '');
 
 -- ----------------------------
 -- Table structure for `main_timeline`
