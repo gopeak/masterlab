985239ec9fb5d5e842dcfc72da5f8b6e90704c96
sven <121642038@qq.com>
2019-12-12 00:07:05 +0800

    开始整合自定义面板的前端

--- app/public/dev/js/agile/board_setting.js
+++ app/public/dev/js/agile/board_setting.js
@@ -237,8 +237,8 @@ var BoardSetting = (function () {
                         {i:3, name:"xxxxxx3", source_status:[], source_resolve:[],source_label:[],source_assignee:[]}
                     ]
                 }
-                //var result = template( columns_data);
-                //$('#xxxxxxxxx').html(result);
+                let result = template( board_data);
+                //$('#board_swim_render').html(result);
 
             },
             error: function (res) {
@@ -249,24 +249,145 @@ var BoardSetting = (function () {
 
     BoardSetting.prototype.showCreateBoardById = function (board_id) {
 
+        $('#form_board_id').val(board_id);
         $('#board_table').hide();
         $('#action-board_list').hide();
         $('#board_add_form').show();
         $('#action-board_form').show();
 
-        var source = $('#board_column_tpl').html();
-        var template = Handlebars.compile(source);
+        let source = $('#board_column_tpl').html();
+        let template = Handlebars.compile(source);
         let board_data = {
             name:"",
-
+            board_range:"all",
+            range_data:"",
+            is_filter_backlog:"0",
+            is_filter_closed:"0",
             columns:[
-                {i:1, name:"xxxxxx1", data:{status:[], resolve:[],label:[],assignee:[]}},
-                {i:2, name:"xxxxxx2", data:{status:[], resolve:[],label:[],assignee:[]}},
-                {i:3, name:"xxxxxx3", data:{status:[], resolve:[],label:[],assignee:[]}}
+                {i:0, name:"准备中", data:{status:['open','reopen','in_review','delay'], resolve:[],label:[],assignee:[]}},
+                {i:1, name:"进行中", data:{status:['in_progress'], resolve:[],label:[],assignee:[]}},
+                {i:2, name:"已解决", data:{status:['closed','done'], resolve:[],label:[],assignee:[]}}
             ]
         }
         let result = template( board_data);
-        //$('#xxxxxxxxx').html(result);
+        $('#board_swim_render').html(result);
+        BoardSetting.prototype.renderSwim(board_data);
+
+    }
+
+    BoardSetting.prototype.renderSwim = function (board_data) {
+        console.log(board_data)
+        let board_name = board_data.name;
+        $('#board_name').val(board_name);
+        let columns = board_data.columns;
+        let is_display_backlog = board_data.is_filter_backlog;
+        let is_display_closed = board_data.is_filter_closed;
+
+        if(is_display_backlog==='1'){
+            $('#checkbox_is_filter_backlog').attr("checked", true);
+        }else{
+            $('#checkbox_is_filter_backlog').removeAttr("checked");
+        }
+        if(is_display_closed==='1'){
+            $('#checkbox_is_filter_closed').attr("checked", true);
+        }else{
+            $('#checkbox_is_filter_closed').removeAttr("checked");
+        }
+
+        for (let i = 0; i < columns.length; i++) {
+            console.log(columns[i].name)
+            let name = columns[i].name;
+            let swim_data = columns[i].data;
+            $('#name_column_'+columns[i].i).val(name);
+            $('#title_column_'+columns[i].i).html(name);
+            BoardSetting.prototype.renderSwimSelect(swim_data, i);
+        }
+        $('.selectpicker').selectpicker('refresh');
+    }
+
+    BoardSetting.prototype.renderSwimSelect = function (swim_data, i ) {
+        console.log(swim_data)
+        if(swim_data){
+            let selects = {};
+            for (let source in  swim_data) {
+                if(source==='status'){
+                    let source_data = swim_data[source];
+                    selects[source] = $('#select_'+source+'_column_' + i);
+                    selects[source].empty();
+                    let select_datas = window._issueConfig.issue_status;
+                    for (let _k in  select_datas) {
+                        let row = select_datas[_k];
+                        let value = row._key;
+                        let title = row.name;
+                        let selected = '';
+                        if (("undefined" !== typeof swim_data[source]) && isInArray(source_data, value)) {
+                            selected = 'selected';
+                        }
+                        let opt = "<option  value='" + value + "' "+selected+" >" + title + "</option>";
+                        console.log(opt)
+                        selects[source].append(opt);
+                    }
+                }
+                if(source==='resolve'){
+                    let source_data = swim_data[source];
+                    selects[source] = $('#select_'+source+'_column_' + i);
+                    selects[source].empty();
+                    let select_datas = window._issueConfig.issue_resolve;
+                    for (let _k in  select_datas) {
+                        let row = select_datas[_k];
+                        let value = row._key;
+                        let title = row.name;
+                        let selected = '';
+                        if (("undefined" !== typeof swim_data[source]) && isInArray(source_data, value)) {
+                            selected = 'selected';
+                        }
+                        let opt = "<option  value='" + value + "' "+selected+" >" + title + "</option>";
+                        console.log(opt)
+                        selects[source].append(opt);
+                    }
+                }
+                if(source==='label'){
+                    let source_data = swim_data.label;
+                    selects[source] = $('#select_'+source+'_column_' + i);
+                    selects[source].empty();
+                    let select_datas = window._issueConfig.issue_labels;
+                    for (let _k in  select_datas) {
+                        let row = select_datas[_k];
+                        let value = _k;
+                        let title = row.title;
+                        let selected = '';
+                        if (("undefined" !== typeof swim_data[source]) && isInArray(source_data, value)) {
+                            selected = 'selected';
+                        }
+                        let opt = "<option  value='" + value + "' "+selected+" >" + title + "</option>";
+                        console.log(opt)
+                        selects[source].append(opt);
+                    }
+                }
+                if(source==='assignee'){
+                    let source_data = swim_data[source];
+                    selects[source] = $('#select_'+source+'_column_' + i);
+                    selects[source].empty();
+                    let select_datas = window._issueConfig.users;
+                    for (let _k in  select_datas) {
+                        let row = select_datas[_k];
+                        let value = row.uid;
+                        let title = row.display_name;
+                        let avatar = row.avatar;
+                        let selected = '';
+                        if (("undefined" !== typeof swim_data[source]) && isInArray(source_data, value)) {
+                            selected = 'selected';
+                        }
+                        let content = "<img width='26px' height='26px' class=' float-none' style='border-radius: 50%;' src='"+avatar+"' > "+title;
+                        let opt  ='<option value="'+value+'"  data-content="'+content+'"  '+selected+'>'+title+'</option>';
+                        console.log(opt)
+                        selects[source].append(opt);
+                    }
+                }
+            }
+        }
+
+
     }
 
 
