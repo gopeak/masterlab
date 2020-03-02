3aa033b8d760748bbb537aac51efcce5aa1745ac
Colder <8162782@qq.com>
2020-03-02 22:05:46 +0800

    补丁包分离索引修改，新增数据库备份和恢复

--- upgrade/v1.2-v2.0-upgrade/data/v1.2-2.0.sql
+++ upgrade/v1.2-v2.0-upgrade/data/v1.2-2.0.sql
@@ -40,8 +40,7 @@ DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
 ROW_FORMAT=Dynamic
 ;
 ALTER TABLE `issue_main` DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-ALTER TABLE `issue_main` DROP INDEX `fulltext_summary_description`;
-ALTER TABLE `issue_main` DROP INDEX `fulltext_summary`;
+
 ALTER TABLE `issue_main` ADD COLUMN `duration`  int(11) UNSIGNED NOT NULL DEFAULT 0 AFTER `due_date`;
 ALTER TABLE `issue_main` ADD COLUMN `level`  tinyint(2) UNSIGNED NOT NULL DEFAULT 0 COMMENT '甘特图级别' AFTER `assistants`;
 ALTER TABLE `issue_main` ADD COLUMN `progress`  tinyint(2) UNSIGNED NOT NULL DEFAULT 0 COMMENT '完成百分比' AFTER `comment_count`;
@@ -50,10 +49,8 @@ ALTER TABLE `issue_main` ADD COLUMN `gant_sprint_weight`  int(11) NOT NULL DEFAU
 ALTER TABLE `issue_main` ADD COLUMN `gant_hide`  tinyint(1) NOT NULL DEFAULT 0 COMMENT '甘特图中是否隐藏该事项' AFTER `gant_sprint_weight`;
 ALTER TABLE `issue_main` ADD COLUMN `is_start_milestone`  tinyint(1) UNSIGNED NOT NULL DEFAULT 0 AFTER `gant_hide`;
 ALTER TABLE `issue_main` ADD COLUMN `is_end_milestone`  tinyint(1) UNSIGNED NOT NULL DEFAULT 0 AFTER `is_start_milestone`;
-DROP INDEX `summary` ON `issue_main`;
-CREATE INDEX `summary` ON `issue_main`(`summary`) USING BTREE ;
-CREATE INDEX `status` ON `issue_main`(`status`) USING BTREE ;
-CREATE INDEX `gant_sprint_weight` ON `issue_main`(`gant_sprint_weight`) USING BTREE ;
+
+
 ALTER TABLE `issue_status` ADD COLUMN `text_color`  varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'black' COMMENT '字体颜色' AFTER `color`;
 ALTER TABLE `main_activity` ADD COLUMN `content`  varchar(1024) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '内容' AFTER `action`;
 ALTER TABLE `main_activity` MODIFY COLUMN `title`  varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '相关的事项标题' AFTER `obj_id`;
@@ -168,15 +165,11 @@ ROW_FORMAT=Dynamic
 -- 下面这一句有变动（原代码为删除default_role_id字段，新增role_id字段，会丢失数据）
 ALTER TABLE `permission_default_role_relation` CHANGE COLUMN `default_role_id` `role_id`  int(11) UNSIGNED NULL DEFAULT NULL AFTER `id`;
 
-DROP INDEX `default_role_id` ON `permission_default_role_relation`;
-CREATE INDEX `default_role_id` ON `permission_default_role_relation`(`role_id`) USING BTREE ;
-DROP INDEX `role_id-and-perm_id` ON `permission_default_role_relation`;
-CREATE INDEX `role_id-and-perm_id` ON `permission_default_role_relation`(`role_id`, `perm_id`) USING BTREE ;
-ALTER TABLE `permission_global` DROP INDEX `_key`;
+
 ALTER TABLE `permission_global` ADD COLUMN `parent_id`  int(11) UNSIGNED NULL DEFAULT 0 AFTER `id`;
 ALTER TABLE `permission_global` MODIFY COLUMN `name`  varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `parent_id`;
 ALTER TABLE `permission_global` MODIFY COLUMN `description`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `name`;
-CREATE INDEX `permission_global_key_idx` ON `permission_global`(`_key`) USING BTREE ;
+
 ALTER TABLE `permission_global` MODIFY COLUMN `id`  int(11) UNSIGNED NOT NULL AUTO_INCREMENT FIRST ;
 CREATE TABLE `permission_global_role` (
 `id`  int(11) UNSIGNED NOT NULL AUTO_INCREMENT ,
@@ -230,8 +223,7 @@ ROW_FORMAT=Dynamic
 ;
 ALTER TABLE `project_label` ADD COLUMN `description`  varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `bg_color`;
 ALTER TABLE `project_main` DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-ALTER TABLE `project_main` DROP INDEX `fulltext_name`;
-ALTER TABLE `project_main` DROP INDEX `fulltext_name_description`;
+
 ALTER TABLE `project_main` ADD COLUMN `archived`  enum('Y','N') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'N' COMMENT '已归档' AFTER `closed_count`;
 ALTER TABLE `project_main` ADD COLUMN `issue_update_time`  int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '事项最新更新时间' AFTER `archived`;
 ALTER TABLE `project_main_extra` DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci;
@@ -253,7 +245,7 @@ ALTER TABLE `project_module` ADD COLUMN `order_weight`  int(10) UNSIGNED NOT NUL
 -- 删除创建表project_permission的语句
 
 -- 修改下面这一句，去掉一个方括号
-CREATE INDEX `project_id` ON `project_role`(`project_id`) USING BTREE ;
+
 ALTER TABLE `service_config` DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci;
 ALTER TABLE `service_config` MODIFY COLUMN `clazz`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `delaytime`;
 ALTER TABLE `service_config` MODIFY COLUMN `servicename`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `clazz`;
