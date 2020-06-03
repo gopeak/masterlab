10a559096da4c292e8a09e1736b1cbe43da8f7f7
sven <121642038@qq.com>
2020-06-03 23:15:00 +0800

    修复安装session路径判断

--- app/public/install/include/var.php
+++ app/public/install/include/var.php
@@ -1,23 +1,25 @@
 <?php
 $env_items = array();
 $dirfile_items = array(
-    array('type' => 'dir', 'path' => realpath(ROOT_PATH . '/../../app/public/install/data')),
-    array('type' => 'dir', 'path' => realpath(ROOT_PATH . '/../../app/public/install')),
-    array('type' => 'dir', 'path' => realpath(ROOT_PATH . '/../../app/public/attachment')),
-    array('type' => 'dir', 'path' => realpath(ROOT_PATH . '/../../upgrade')),
-    array('type' => 'dir', 'path' => realpath(ROOT_PATH . '/../../app/storage')),
-    array('type' => 'file', 'path' => realpath(ROOT_PATH . '/../../app/config/deploy/app.cfg.php')),
-    array('type' => 'file', 'path' => realpath(ROOT_PATH . '/../../app/config/deploy/database.cfg.php')),
-    array('type' => 'file', 'path' => realpath(ROOT_PATH . '/../../app/config/deploy/cache.cfg.php')),
-    array('type' => 'file', 'path' => realpath(ROOT_PATH . '/../../bin/config.toml')),
-    array('type' => 'file', 'path' => realpath(ROOT_PATH . '/../../bin/cron.json')),
+    array('type' => 'dir', 'path' => realpath(INSTALL_PATH . '/../../app/public/install/data')),
+    array('type' => 'dir', 'path' => realpath(INSTALL_PATH . '/../../app/public/install')),
+    array('type' => 'dir', 'path' => realpath(INSTALL_PATH . '/../../app/public/attachment')),
+    array('type' => 'dir', 'path' => realpath(INSTALL_PATH . '/../../upgrade')),
+    array('type' => 'dir', 'path' => realpath(INSTALL_PATH . '/../../app/storage')),
+    array('type' => 'file', 'path' => realpath(INSTALL_PATH . '/../../config.yml')),
+    array('type' => 'file', 'path' => realpath(INSTALL_PATH . '/../../bin/config.toml')),
+    array('type' => 'file', 'path' => realpath(INSTALL_PATH . '/../../bin/cron.json')),
 );
 if (ini_get('session.save_handler') == 'files') {
     $sessionPath = session_save_path();
     if (strpos($sessionPath, ";") !== false) {
         $sessionPath = substr($sessionPath, strpos($sessionPath, ";") + 1);
-        $dirfile_items[] = ['type' => 'file', 'path' => realpath($sessionPath)];
     }
+    if(file_exists($sessionPath)){
+        $sessionPath = realpath($sessionPath);
+        $dirfile_items[] = ['type' => 'file', 'path' => $sessionPath];
+    }
+
 }
 
 $func_items = array(
