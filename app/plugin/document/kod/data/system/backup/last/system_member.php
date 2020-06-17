07e09c49fc36febf5380702be9d7554c28fa252b
sven <121642038@qq.com>
2020-06-18 00:01:52 +0800

    add kod sdk class

--- app/plugin/document/kod/data/system/backup/last/system_member.php
+++ app/plugin/document/kod/data/system/backup/last/system_member.php
@@ -14,13 +14,13 @@
         },
         "createTime": 1589187576,
         "status": 1,
-        "lastLogin": 1591178771
+        "lastLogin": 1592312938
     },
     "100": {
         "userID": "100",
         "name": "demo",
         "password": "fe01ce2a7fbac8fafaed7c982a04e229",
-        "role": "2",
+        "role": "1",
         "config": {
             "sizeMax": 5,
             "sizeUse": 1048576
@@ -30,8 +30,10 @@
         },
         "path": "demo",
         "status": 1,
-        "lastLogin": "",
-        "createTime": 1589187576
+        "lastLogin": 1592312224,
+        "createTime": 1589187576,
+        "nickName": "Demo用户",
+        "sizeMax": "5"
     },
     "101": {
         "userID": "101",
@@ -49,5 +51,383 @@
         "status": 1,
         "lastLogin": "",
         "createTime": 1589187576
+    },
+    "102": {
+        "userID": "102",
+        "name": "79720699",
+        "nickName": "79720699",
+        "password": "e10adc3949ba59abbe56e057f20f883e",
+        "role": "1",
+        "config": {
+            "sizeMax": 2,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "79720699",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1592315337
+    },
+    "103": {
+        "userID": "103",
+        "name": "121",
+        "nickName": "121",
+        "password": "e10adc3949ba59abbe56e057f20f883e",
+        "role": "1",
+        "config": {
+            "sizeMax": 2,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "121",
+        "status": 1,
+        "lastLogin": 1592393276,
+        "createTime": 1592315346
+    },
+    "104": {
+        "userID": "104",
+        "name": "122",
+        "nickName": "122",
+        "password": "e10adc3949ba59abbe56e057f20f883e",
+        "role": "1",
+        "config": {
+            "sizeMax": 2,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "122",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1592315350
+    },
+    "105": {
+        "userID": "105",
+        "name": "123",
+        "nickName": "123",
+        "password": "e10adc3949ba59abbe56e057f20f883e",
+        "role": "1",
+        "config": {
+            "sizeMax": 2,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "123",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1592315355
+    },
+    "106": {
+        "userID": "106",
+        "name": "125",
+        "nickName": "125",
+        "password": "e10adc3949ba59abbe56e057f20f883e",
+        "role": "1",
+        "config": {
+            "sizeMax": 2,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "125",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1592315367
+    },
+    "107": {
+        "userID": "107",
+        "name": "126",
+        "nickName": "126",
+        "password": "e10adc3949ba59abbe56e057f20f883e",
+        "role": "1",
+        "config": {
+            "sizeMax": 2,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "126",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1592315395
+    },
+    "108": {
+        "userID": "108",
+        "name": "127",
+        "nickName": "127",
+        "password": "e10adc3949ba59abbe56e057f20f883e",
+        "role": "1",
+        "config": {
+            "sizeMax": 2,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "127",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1592315395
+    },
+    "109": {
+        "userID": "109",
+        "name": "128",
+        "nickName": "128",
+        "password": "e10adc3949ba59abbe56e057f20f883e",
+        "role": "1",
+        "config": {
+            "sizeMax": 2,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "128",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1592315395
+    },
+    "110": {
+        "userID": "110",
+        "name": "129",
+        "nickName": "129",
+        "password": "e10adc3949ba59abbe56e057f20f883e",
+        "role": "1",
+        "config": {
+            "sizeMax": 2,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "129",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1592315395
+    },
+    "111": {
+        "userID": "111",
+        "name": "130",
+        "nickName": "130",
+        "password": "e10adc3949ba59abbe56e057f20f883e",
+        "role": "1",
+        "config": {
+            "sizeMax": 2,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "130",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1592315395
+    },
+    "112": {
+        "userID": "112",
+        "name": "140",
+        "nickName": "140",
+        "password": "e10adc3949ba59abbe56e057f20f883e",
+        "role": "1",
+        "config": {
+            "sizeMax": 2,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "140",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1592315395
+    },
+    "113": {
+        "userID": "113",
+        "name": "190",
+        "nickName": "190",
+        "password": "e10adc3949ba59abbe56e057f20f883e",
+        "role": "1",
+        "config": {
+            "sizeMax": 2,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "190",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1592315421
+    },
+    "114": {
+        "userID": "114",
+        "name": "191",
+        "nickName": "191",
+        "password": "e10adc3949ba59abbe56e057f20f883e",
+        "role": "1",
+        "config": {
+            "sizeMax": 2,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "191",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1592315421
+    },
+    "115": {
+        "userID": "115",
+        "name": "192",
+        "nickName": "192",
+        "password": "e10adc3949ba59abbe56e057f20f883e",
+        "role": "1",
+        "config": {
+            "sizeMax": 2,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "192",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1592315421
+    },
+    "116": {
+        "userID": "116",
+        "name": "193",
+        "nickName": "193",
+        "password": "e10adc3949ba59abbe56e057f20f883e",
+        "role": "1",
+        "config": {
+            "sizeMax": 2,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "193",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1592315421
+    },
+    "117": {
+        "userID": "117",
+        "name": "194",
+        "nickName": "194",
+        "password": "e10adc3949ba59abbe56e057f20f883e",
+        "role": "1",
+        "config": {
+            "sizeMax": 2,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "194",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1592315421
+    },
+    "118": {
+        "userID": "118",
+        "name": "195",
+        "nickName": "195",
+        "password": "e10adc3949ba59abbe56e057f20f883e",
+        "role": "1",
+        "config": {
+            "sizeMax": 2,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "195",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1592315421
+    },
+    "119": {
+        "userID": "119",
+        "name": "196",
+        "nickName": "196",
+        "password": "e10adc3949ba59abbe56e057f20f883e",
+        "role": "1",
+        "config": {
+            "sizeMax": 2,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "196",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1592315421
+    },
+    "120": {
+        "userID": "120",
+        "name": "197",
+        "nickName": "197",
+        "password": "e10adc3949ba59abbe56e057f20f883e",
+        "role": "1",
+        "config": {
+            "sizeMax": 2,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "197",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1592315421
+    },
+    "121": {
+        "userID": "121",
+        "name": "198",
+        "nickName": "198",
+        "password": "e10adc3949ba59abbe56e057f20f883e",
+        "role": "1",
+        "config": {
+            "sizeMax": 2,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "198",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1592315421
+    },
+    "122": {
+        "userID": "122",
+        "name": "199",
+        "nickName": "199",
+        "password": "e10adc3949ba59abbe56e057f20f883e",
+        "role": "1",
+        "config": {
+            "sizeMax": 2,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "199",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1592315421
     }
 }
