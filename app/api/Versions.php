8ac5b6fd5d64643ddde8f889c8bb0c971b5093f0
Tony <cheerfultony@gmail.com>
2020-07-24 21:00:12 +0800

    add openapi sprints

--- app/api/Versions.php
+++ app/api/Versions.php
@@ -95,35 +95,125 @@ class Versions extends BaseAuth
             $projectId = intval($_GET['project_id']);
         }
 
-        if (!isset($_POST['label_name'])) {
+        if (!isset($_POST['version_name'])) {
             return self::returnHandler('需要版本名.', [], Constants::HTTP_BAD_REQUEST);
         }
-        $labelName = trim($_POST['label_name']);
+        $versionName = trim($_POST['version_name']);
 
         if (!isset($_POST['description'])) {
             $_POST['description'] = '';
         }
 
-        $projectLabelModel = new ProjectLabelModel($uid);
+        $info = [];
+        $info['project_id'] = $projectId;
+        $info['name'] = $versionName;
+        $info['description'] = $_POST['description'];
+        $info['sequence'] = 0;
 
-        if ($projectLabelModel->checkNameExist($projectId, $labelName)) {
+        $projectVersionModel = new ProjectVersionModel();
+        if ($projectVersionModel->checkNameExist($projectId, $versionName)) {
             return self::returnHandler('name is exist.', [], Constants::HTTP_BAD_REQUEST);
         }
-
-        $row = [];
-        $row['project_id'] = $projectId;
-        $row['title'] = $labelName;
-        $row['color'] = '#FFFFFF';
-        $row['bg_color'] = '#428BCA';
-        $row['description'] = $_POST['description'];
-
-        $ret = $projectLabelModel->insert($row);
+        $ret = $projectVersionModel->insert($info);
 
         if ($ret[0]) {
             return self::returnHandler('操作成功', ['id' => $ret[1]]);
         } else {
-            return self::returnHandler('添加标签失败.', [], Constants::HTTP_BAD_REQUEST);
+            return self::returnHandler('添加版本失败.', [], Constants::HTTP_BAD_REQUEST);
         }
+    }
+
+    /**
+     * Restful DELETE ,删除某个项目模块
+     * {{API_URL}}/api/versions/v1/36?access_token==xyz
+     * @return array
+     * @throws \Doctrine\DBAL\ConnectionException
+     * @throws \Doctrine\DBAL\DBALException
+     * @throws \Doctrine\DBAL\Exception\InvalidArgumentException
+     * @throws \Exception
+     */
+    private function deleteHandler()
+    {
+        $uid = $this->masterUid;
+        $versionId = 0;
+        $projectId = 0;
+
+        if (isset($_GET['_target'][3])) {
+            $versionId = intval($_GET['_target'][3]);
+        }
+        if ($versionId == 0) {
+            return self::returnHandler('需要有版本ID', [], Constants::HTTP_BAD_REQUEST);
+        }
+
+        if (isset($_GET['project_id'])) {
+            $projectId = intval($_GET['project_id']);
+        }
+        if ($projectId == 0) {
+            return self::returnHandler('需要有项目ID', [], Constants::HTTP_BAD_REQUEST);
+        }
+
+        $projectVersionModel = new ProjectVersionModel();
+        $projectVersionModel->deleteByVersinoId($projectId, $versionId);
+
+        return self::returnHandler('操作成功');
+    }
+
+    /**
+     * Restful PATCH ,更新项目版本
+     * {{API_URL}}/api/versions/v1/36?access_token==xyz
+     * @return array
+     * @throws \Exception
+     */
+    private function patchHandler()
+    {
+        $uid = $this->masterUid;
+        $versionId = 0;
+        $projectId = 0;
+
+        if (isset($_GET['_target'][3])) {
+            $versionId = intval($_GET['_target'][3]);
+        }
+        if ($versionId == 0) {
+            return self::returnHandler('需要有版本ID', [], Constants::HTTP_BAD_REQUEST);
+        }
+
+        if (isset($_GET['project_id'])) {
+            $projectId = intval($_GET['project_id']);
+        }
+        if ($projectId == 0) {
+            return self::returnHandler('需要有项目ID', [], Constants::HTTP_BAD_REQUEST);
+        }
+
+        $patch = self::_PATCH();
+
+        $row = [];
+        if (isset($patch['version_name']) && !empty($patch['version_name'])) {
+            $row['name'] = $patch['version_name'];
+        } else {
+            return self::returnHandler('需要有模块名', [], Constants::HTTP_BAD_REQUEST);
+        }
+
+        if (isset($patch['description']) && !empty($patch['description'])) {
+            $row['description'] = $patch['description'];
+        }
+
+        if (isset($patch['released']) && !empty($patch['released']) && in_array($patch['released'], ['0','1'])) {
+            $row['released'] = $patch['released'];
+        }
+
+        if (isset($patch['start_date']) && !empty($patch['start_date']) && is_datetime_format($patch['start_date'])) {
+            $row['start_date'] = strtotime($patch['start_date']);
+        }
+
+        if (isset($patch['release_date']) && !empty($patch['release_date']) && is_datetime_format($patch['release_date'])) {
+            $row['release_date'] = strtotime($patch['release_date']);
+        }
+
+        $projectModuleModel = new ProjectVersionModel();
+        $ret = $projectModuleModel->updateById($versionId, $row);
+
+        return self::returnHandler('修改成功', array_merge($row, ['id' => $versionId]));
+
     }
 
 }
