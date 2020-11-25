b70f9105478f462b518f5bbea2b5e8f398f4a401
sven <121642038@qq.com>
2020-11-25 17:36:52 +0800

    修复crontab 表达式异常的问题

--- bin/swoole_server.php
+++ bin/swoole_server.php
@@ -1,15 +1,16 @@
 <?php
 require_once './bootstrap.php';
 require_once $rootDir . '/vendor/autoload.php';
+
 use main\app\model\SettingModel;
 use main\app\model\system\MailQueueModel;
 
 $socketConfig = $GLOBALS['_yml_config']['socket'];
 print_r($socketConfig);
-if($socketConfig){
+if ($socketConfig) {
     $socketHost = trimStr($socketConfig['host']);
     $socketPort = (int)$socketConfig['port'];
-}else{
+} else {
     $socketHost = trimStr($config['socket_server_host']);
     $socketPort = (int)$config['socket_server_port'];
 }
@@ -22,14 +23,14 @@ $server->set(array('task_worker_num' => 12));
 $server->set([
     'daemonize' => false, //是否作为守护进程
     'open_length_check' => true,
-    'package_max_length' => 1024*1024*10,
+    'package_max_length' => 1024 * 1024 * 10,
     'package_length_type' => 'N', //see php pack()
     'package_length_offset' => 0,
     'package_body_offset' => 4,
 ]);
 
 //此回调函数在worker进程中执行
-$server->on('receive', function($serv, $fd, $from_id, $data) {
+$server->on('receive', function ($serv, $fd, $from_id, $data) {
     $len = unpack('N', $data)['1'];
     $body = substr($data, -$len);
     //投递异步任务
@@ -40,13 +41,13 @@ $server->on('receive', function($serv, $fd, $from_id, $data) {
 //处理异步任务(此回调函数在task进程中执行)
 $server->on('task', function ($serv, $task_id, $from_id, $data) {
 
-   // echo "New AsyncTask[id=$task_id]".PHP_EOL;
+    // echo "New AsyncTask[id=$task_id]".PHP_EOL;
     $bodyArr = json_decode($data, true);
     $sendArr = $bodyArr;
     print_r($sendArr);
     $recipients = $sendArr['to'];
     $replyTo = $sendArr['cc'];
-    $title = $sendArr['subject'] ;
+    $title = $sendArr['subject'];
     $content = $sendArr['body'];
     $sendArr['attach'] = isset($sendArr['attach']) ? $sendArr['attach'] : '';
 
@@ -68,7 +69,7 @@ $server->on('task', function ($serv, $task_id, $from_id, $data) {
         $mail->CharSet = 'UTF-8'; //设置邮件的字符编码，这很重要，不然中文乱码
         $mail->SMTPAuth = true; //开启认证
         $mail->Port = (int)$sendArr['port'];
-        $mail->SMTPDebug =  0;
+        $mail->SMTPDebug = 0;
         $mail->Host = $sendArr['host'];
         $mail->Username = $sendArr['user'];
         $mail->Password = $sendArr['password'];
@@ -99,7 +100,7 @@ $server->on('task', function ($serv, $task_id, $from_id, $data) {
                 'allow_self_signed' => true
             )
         );
-        if(is_string($recipients)){
+        if (is_string($recipients)) {
             $recipients = [$recipients];
         }
         foreach ($recipients as $addr) {
@@ -111,7 +112,7 @@ $server->on('task', function ($serv, $task_id, $from_id, $data) {
         }
         $mail->Subject = $title;
         $mail->Body = $content;
-        if(is_string($replyTo)){
+        if (is_string($replyTo)) {
             $replyTo = [$replyTo];
         }
         if (!empty($replyTo)) {
@@ -121,7 +122,7 @@ $server->on('task', function ($serv, $task_id, $from_id, $data) {
                 }
             }
         }
-        if(isset($sendArr['attach']) && !empty($sendArr['attach'])){
+        if (isset($sendArr['attach']) && !empty($sendArr['attach'])) {
             $mail->addAttachment($sendArr['attach']);
         }
         $mail->AltBody = "To view the message, please use an HTML compatible email viewer!"; //当邮件不支持html时备用显示，可以省略
@@ -131,21 +132,21 @@ $server->on('task', function ($serv, $task_id, $from_id, $data) {
         $ret = $mail->Send();
         if (!$ret) {
             $msg = 'Mailer Error: ' . $mail->ErrorInfo;
-            $mailQueModel->update(['status'=>MailQueueModel::STATUS_ERROR, 'error'=>$msg], ['seq'=>$sendArr['seq']]);
+            $mailQueModel->update(['status' => MailQueueModel::STATUS_ERROR, 'error' => $msg], ['seq' => $sendArr['seq']]);
         }
     } catch (\phpmailerException $e) {
         // print_r($e->getCode());
         //print_r($e->getTrace());
         $msg = "邮件发送失败：" . $e->errorMessage();
         echo $msg;
-        $mailQueModel->update(['status'=>MailQueueModel::STATUS_ERROR, 'error'=>$msg], ['seq'=>$sendArr['seq']]);
+        $mailQueModel->update(['status' => MailQueueModel::STATUS_ERROR, 'error' => $msg], ['seq' => $sendArr['seq']]);
 
     } catch (\Exception $e) {
         $msg = "邮件发送失败：" . $e->getMessage();
         echo $msg;
-        $mailQueModel->update(['status'=>MailQueueModel::STATUS_ERROR, 'error'=>$msg], ['seq'=>$sendArr['seq']]);
+        $mailQueModel->update(['status' => MailQueueModel::STATUS_ERROR, 'error' => $msg], ['seq' => $sendArr['seq']]);
     }
-    $mailQueModel->update(['status'=>MailQueueModel::STATUS_DONE, 'error'=>''], ['seq'=>$sendArr['seq']]);
+    $mailQueModel->update(['status' => MailQueueModel::STATUS_DONE, 'error' => ''], ['seq' => $sendArr['seq']]);
     //返回任务执行的结果
     $serv->finish("$data -> OK");
 });
@@ -153,56 +154,61 @@ $server->on('task', function ($serv, $task_id, $from_id, $data) {
 
 //处理异步任务的结果(此回调函数在worker进程中执行)
 $server->on('finish', function ($serv, $task_id, $data) {
-    echo "AsyncTask[$task_id] Finished ".PHP_EOL;
+    echo "AsyncTask[$task_id] Finished " . PHP_EOL;
 });
 
 
-Swoole\Timer::tick(1000, function(){
+Swoole\Timer::tick(1000, function () {
 
     echo "crontab checking \n";
-    $rootDir = realpath(dirname(__FILE__). '/../') ;
-    $json = file_get_contents($rootDir.'/bin/cron.json');
+    $rootDir = realpath(dirname(__FILE__) . '/../');
+    $json = file_get_contents($rootDir . '/bin/cron.json');
     $cronArr = json_decode($json, true);
-    if(!$cronArr['schedule']){
+    if (!$cronArr['schedule']) {
         return;
     }
     foreach ($cronArr['schedule'] as $item) {
-        $exp = substr($item['exp'],2);
-        //echo $exp." ";
-        $cron = Cron\CronExpression::factory($exp);
-        //echo $cron->getNextRunDate()->format('Y-m-d H:i:s')." \n";
-        $runTime = $cron->getNextRunDate()->getTimestamp();
-        $offsetTime = $runTime-time();
-        if($offsetTime<2 && $offsetTime>-2){
-            log_cron("脚本:".$item['name']." ".$item['file']." 触发");
-            echo $item['name'].' '.$cron->getNextRunDate()->format('Y-m-d H:i:s')." \n";
-            // 达到时间要求
-            $cronPhpBin = $item['exe_bin'];
-            if(!file_exists($cronPhpBin)){
-                list($phpBinRet, $phpBin) = get_php_bin_dir();
-                if(!$phpBinRet){
+        $exp = substr($item['exp'], 2);
+        $exp = str_replace('?', '*', $exp);
+        //echo $exp . " " . PHP_EOL;
+        try {
+            $cron = Cron\CronExpression::factory($exp);
+            //echo $cron->getNextRunDate()->format('Y-m-d H:i:s')." \n";
+            $runTime = $cron->getNextRunDate()->getTimestamp();
+            $offsetTime = $runTime - time();
+            if ($offsetTime < 2 && $offsetTime > -2) {
+                log_cron("脚本:" . $item['name'] . " " . $item['file'] . " 触发");
+                echo $item['name'] . ' ' . $cron->getNextRunDate()->format('Y-m-d H:i:s') . " \n";
+                // 达到时间要求
+                $cronPhpBin = $item['exe_bin'];
+                if (!file_exists($cronPhpBin)) {
+                    list($phpBinRet, $phpBin) = get_php_bin_dir();
+                    if (!$phpBinRet) {
+                        continue;
+                    }
+                    $cronPhpBin = $phpBin;
+                }
+                $execFile = $item['file'];
+                $pathParts = pathinfo($execFile);
+                if (!file_exists($execFile)) {
+                    $newFile = $rootDir . '/app/server/timer/' . $pathParts['basename'];
+                    if (file_exists($newFile)) {
+                        $execFile = $newFile;
+                    }
+                }
+                exec("ps aux | grep " . $pathParts['basename'], $output, $return);
+                if ($return == 0) {
+                    echo $item['file'] . ", process is running\n";
                     continue;
                 }
-                $cronPhpBin = $phpBin;
+                $command = $cronPhpBin . ' ' . $item['arg'] . ' ' . $execFile;
+                log_cron($command);
+                exec($command, $output);
+                log_cron(print_r($output, true));
+                log_cron("脚本:" . $item['name'] . " " . $execFile . " 执行结束");
             }
-            $execFile = $item['file'];
-            $pathParts  = pathinfo($execFile);
-            if(!file_exists($execFile)){
-                $newFile = $rootDir.'/app/server/timer/'.$pathParts['basename'];
-                if(file_exists($newFile)){
-                    $execFile = $newFile;
-                }
-            }
-            exec("ps aux | grep ".$pathParts['basename'], $output, $return);
-            if ($return == 0) {
-                echo $item['file'].", process is running\n";
-                continue;
-            }
-            $command = $cronPhpBin.' '.$item['arg'].' '.$execFile;
-            log_cron($command);
-            exec($command, $output);
-            log_cron(print_r($output, true));
-            log_cron("脚本:".$item['name']." ".$execFile." 执行结束");
+        } catch (\Exception $e) {
+            echo $e->getMessage() . PHP_EOL;
         }
 
     }
