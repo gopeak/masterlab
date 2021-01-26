968bb112bed8a2a30812548e734b0af24f76ae83
sven <121642038@qq.com>
2021-01-26 10:56:23 +0800

    修复在极简模式下的事项分解为空的bug

--- plugin/document/kod/data/system/backup/last/system_member.php
+++ plugin/document/kod/data/system/backup/last/system_member.php
@@ -14,10 +14,46 @@
         },
         "createTime": 1589187576,
         "status": 1,
-        "lastLogin": 1608800518
+        "lastLogin": 1611628386
     },
     "100": {
         "userID": "100",
+        "name": "project92",
+        "nickName": "project92",
+        "password": "2e941280dccb0c5a2526bd7995f7dc19",
+        "role": "2",
+        "config": {
+            "sizeMax": 5,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "project92_100",
+        "status": 1,
+        "lastLogin": 1608802922,
+        "createTime": 1608801240
+    },
+    "101": {
+        "userID": "101",
+        "name": "project93",
+        "nickName": "project93",
+        "password": "e39b8a859be3d0eee9678cdf59096553",
+        "role": "2",
+        "config": {
+            "sizeMax": 5,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "project93_101",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1608801308
+    },
+    "102": {
+        "userID": "102",
         "name": "project1",
         "nickName": "project1",
         "password": "cdc4c5cc90e916bee8a68a49213bcc46",
@@ -29,9 +65,9 @@
         "groupInfo": {
             "1": "write"
         },
-        "path": "project1_100",
+        "path": "project1_102",
         "status": 1,
-        "lastLogin": 1608800519,
-        "createTime": 1608800518
+        "lastLogin": 1611628386,
+        "createTime": 1611628386
     }
 }
